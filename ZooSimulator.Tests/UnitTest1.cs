dfb830ae0c912a8161ca2d3dd7ab0a7aaa675f19
Craig McKay <craig.mckay@gmail.com>
2018-02-11 21:46:08 +0000

    adding dotnet core solution, web api and mstest project

---
+++ ZooSimulator.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZooSimulator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
